31be607f0b2cc2584d3b423d2cd5e7fdb964a5c3
GenosseBlaackberry <51916553+GenosseBlaackberry@users.noreply.github.com>
2021-09-21 22:02:06 +0700

    Update 5.py

--- lab3 (Turtle 2)/5.py
+++ lab3 (Turtle 2)/5.py
@@ -2,26 +2,42 @@ from random import randint
 import turtle
 
 
-number_of_turtles = 5
-steps_of_time_number = 100
+number_of_turtles = 10
+steps_of_time_number = 1000
 
+length = 100
+
+def boxpainting(obj, length):
+    obj.penup()
+    obj.goto(-length, -length)
+    obj.pendown()
+    obj.goto(length, -length)
+    obj.goto(length, length)
+    obj.goto(-length, length)
+    obj.goto(-length, -length)
+
+def wallcheck(obj, length):
+    if abs(unit.xcor())>=(length-10):
+            obj.left(180-2*unit.heading())
+            obj.forward(2)
+    if abs(unit.ycor())>=(length-10):
+            obj.right(2*unit.heading())
+            obj.forward(2)
+    
 
 pool = [turtle.Turtle(shape='turtle') for i in range(number_of_turtles)]
-pool[1].penup()
-pool[1].goto(-300, -300)
-pool[1].pendown()
-pool[1].goto(300, -300)
-pool[1].goto(300, 300)
-pool[1].goto(-300, 300)
-pool[1].goto(-300, -300)
+
+boxpainting(pool[0], length)
+
 for unit in pool:
     unit.penup()
-    unit.speed(50)
-    unit.goto(randint(-200, 200), randint(-200, 200))
+    unit.speed(10)
+    unit.goto(randint(-(length-10), (length-10)), randint(-(length-10), (length-10)))
     unit.left(randint(-180, 180))
 
 
 
 for i in range(steps_of_time_number):
     for unit in pool:
+        wallcheck(unit, length)
         unit.forward(2)

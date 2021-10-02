b099920be880940c2ed75032821b53123baacd3c
Zigismund <nikchersib@gmail.com>
2021-10-02 17:49:45 +0700

    Beardrawing code was put in a function

--- lab 4/2.py
+++ lab 4/2.py
@@ -8,30 +8,53 @@ def whiteball(cd):
     ellipse(screen, (255, 255, 255), (cd[0] + 1, cd[1] + 2, cd[2] - 2, cd[3] - 4))
 
 def fibsh(cd):
-    polygon(screen, (255, 150, 0), ((cd[0] + 10, cd[1]), (cd[0] + 8, cd[1] - 6), (cd[0] + 30, cd[1] - 6), (cd[0] + 28, cd[1])))
-    ellipse(screen, (50, 180, 50), (cd[0], cd[1], 40, 20))
-    polygon(screen, (50, 180, 50), ((cd[0], cd[1] + 8), (cd[0] - 7, cd[1] + 15), (cd[0] - 7, cd[1])))
-    ellipse(screen, (0, 0, 200), (cd[0]+30, cd[1] + 5, 5, 5))
-    polygon(screen, (255, 150, 0), ((cd[0] + 24, cd[1] + 10), (cd[0] + 20, cd[1] + 6), (cd[0] + 20, cd[1] + 14)))
+    cd = (cd[0] / cd[2], cd[1] / cd[2], cd[2])
+    polygon(screen, (255, 150, 0), ((cd[0]*cd[2] + 10*cd[2], (cd[1] + 4)*cd[2]), (cd[0]*cd[2] + 8*cd[2], cd[1]*cd[2] - 2*cd[2]),
+                                    (cd[0]*cd[2] + 30*cd[2], cd[1]*cd[2] - 2*cd[2]), (cd[0]*cd[2] + 28*cd[2], (cd[1]+4)*cd[2])))
+    ellipse(screen, (50, 180, 50), (cd[0]*cd[2], cd[1]*cd[2], 40*cd[2], 20*cd[2]))
+    polygon(screen, (50, 180, 50), ((cd[0]*cd[2], cd[1]*cd[2] + 8*cd[2]), (cd[0]*cd[2] - int(7*cd[2]), cd[1]*cd[2] + 15*cd[2]),
+                                    (cd[0]*cd[2] - int(7*cd[2]), cd[1]*cd[2])))
+    ellipse(screen, (0, 0, 200), (cd[0]*cd[2] + 30*cd[2], cd[1]*cd[2] + 5*cd[2], 5*cd[2], 5*cd[2]))
+    polygon(screen, (255, 150, 0), ((cd[0]*cd[2] + 24*cd[2], cd[1]*cd[2] + 10*cd[2]),
+                                    (cd[0]*cd[2] + 20*cd[2], cd[1]*cd[2] + 6*cd[2]),
+                                    (cd[0]*cd[2] + 20*cd[2], cd[1]*cd[2] + 14*cd[2])))
+
+def bebr(cd):
+    cd = (cd[0]/cd[2], cd[1]/cd[2], cd[2])
+    line(screen, (0, 0, 0), (cd[0]*cd[2] + 160*cd[2], cd[1]*cd[2] + 200*cd[2]),
+         (cd[0]*cd[2] + 310*cd[2], cd[1]*cd[2] - 100*cd[2]), max(int(5*cd[2]), 1))
+    ellipse(screen, (70, 70, 100), (cd[0]*cd[2] + 260*cd[2], cd[1]*cd[2] + 210*cd[2], 120*cd[2], 50*cd[2]))
+    ellipse(screen, (50, 50, 200), (cd[0]*cd[2] + 270*cd[2], cd[1]*cd[2] + 220*cd[2], 100*cd[2], 40*cd[2]))
+    line(screen, (0, 0, 0), (cd[0]*cd[2] + 310*cd[2], cd[1]*cd[2] - 100*cd[2]),
+         (cd[0]*cd[2] + 310*cd[2], cd[1]*cd[2] + 240*cd[2]), 1)
+    whiteball((cd[0]*cd[2], cd[1]*cd[2], 150*cd[2], 300*cd[2]))
+    whiteball((cd[0]*cd[2] + 70*cd[2], cd[1]*cd[2] + 240*cd[2], 120*cd[2], 80*cd[2]))
+    whiteball((cd[0]*cd[2] + 150*cd[2], cd[1]*cd[2] + 290*cd[2], 60*cd[2], 40*cd[2]))
+    whiteball((cd[0]*cd[2] + 120*cd[2], cd[1]*cd[2] + 80*cd[2], 100*cd[2], 60*cd[2]))
+    whiteball((cd[0]*cd[2] + 100*cd[2], cd[1]*cd[2] - 30*cd[2], 100*cd[2], 70*cd[2]))
+    whiteball((cd[0]*cd[2] + 95*cd[2], cd[1]*cd[2] - 24*cd[2], 20*cd[2], 20*cd[2]))
+    line(screen, (0, 0, 0), (cd[0]*cd[2] + 180*cd[2], cd[1]*cd[2] + 5*cd[2]),
+         (cd[0]*cd[2] + 200*cd[2], cd[1]*cd[2] + 10*cd[2]))
+    ellipse(screen, (0, 0, 0), (cd[0]*cd[2] + 190*cd[2], cd[1]*cd[2] - 10*cd[2], 10*cd[2], 10*cd[2]))
+    ellipse(screen, (0, 0, 0), (cd[0]*cd[2] + 120*cd[2], cd[1]*cd[2] - 20*cd[2], 5*cd[2], 5*cd[2]))
+
+    fibsh((cd[0]*cd[2] + 320*cd[2], cd[1]*cd[2] + 300*cd[2], cd[2]))
+
 
 FPS = 30
 screen = pygame.display.set_mode((650, 1000))
 
 rect(screen, (100, 100, 255), (0, 0, 650, 500))
 rect(screen, (255, 255, 255), (0, 500, 650, 500))
-line(screen, (0, 0, 0), (190, 600), (340, 300), 5)
-ellipse(screen, (70, 70, 100), (290, 610, 120, 50))
-ellipse(screen, (50, 50, 200), (300, 620, 100, 40))
-line(screen, (0, 0, 0), (340, 300), (340, 640), 1)
-whiteball((30, 400, 150, 300))
-whiteball((100, 640, 120, 80))
-whiteball((180, 690, 60, 40))
-whiteball((150, 480, 100, 60))
-whiteball((130, 370, 100, 70))
-whiteball((125, 376, 20, 20))
-ellipse(screen, (0, 0, 0), (220, 390, 10, 10))
-ellipse(screen, (0, 0, 0), (170, 380, 5, 5))
-fibsh((350, 700))
+
+
+bebr((100, 400, 1))
+
+ellipse(screen, (255, 255, 0), (305, 130, 60, 60))
+ellipse(screen, (100, 100, 255), (310, 135, 50, 50))
+ellipse(screen, (255, 255, 0), (325, 150, 20, 20))
+
+
 
 
 
